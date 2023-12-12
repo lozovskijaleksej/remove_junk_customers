c3db12459d3a8f83cb686f4ce77a491573d09391
lozovskijaleksej <lozovskijaleksej03@gmail.com>
2023-12-12 14:56:18 +0000

    update

--- main.py
+++ main.py
@@ -14,29 +14,19 @@ def getCustomers(storeDomain, accessToken):
         email = junkFile.readline()
         if not email:
             break
-        junkEmails.update({email.split()[0]:True})
-    since_id = 0
-    while True:
-        response = requests.get(f'https://{storeDomain}.myshopify.com/admin/api/2023-10/customers.json?since_id={since_id}', headers={'X-Shopify-Access-Token': accessToken})
-        old_since_id = since_id
+        junkEmail = email.split()[0]
+        response = requests.get(f'https://{storeDomain}.myshopify.com/admin/api/2023-10/customers/search.json?query=email:{junkEmail}', headers={'X-Shopify-Access-Token': accessToken})
         if response.status_code != 200:
-            return False
+            log(f'False0:{junkEmail}')
+            continue
         customers = response.json()['customers']
         for customer in customers:
             customer_id = customer.get('id', '')
-            customer_email = customer.get('email', '')
-            try:
-                junkEmails[customer_email]
-                response = requests.delete(f'https://{storeDomain}.myshopify.com/admin/api/2023-10/customers/{customer_id}.json', headers={'X-Shopify-Access-Token': accessToken})
-                if response.status_code == 200:
-                    log(f'{customer_email} : True : True')
-                else:
-                    log(f'{customer_email} : True : False')
-            except:
-                log(f'{customer_email} : False')
-            since_id = customer_id
-        if old_since_id == since_id:
-            break
+            response = requests.delete(f'https://{storeDomain}.myshopify.com/admin/api/2023-10/customers/{customer_id}.json', headers={'X-Shopify-Access-Token': accessToken})
+            if response.status_code != 200:
+                log(f'False1:{junkEmail}')
+            else:
+                log(f'success: {junkEmail}')
 
 
 def main():
