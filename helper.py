b5450c705105c7d047e6da5d56efa4f8d559f2ce
lozovskijaleksej <lozovskijaleksej03@gmail.com>
2023-12-13 17:25:13 +0000

    complete: remove_from_mailchimp

--- helper.py
+++ helper.py
@@ -1,6 +1,13 @@
 from datetime import datetime
+import hashlib
 
 
 def log(message):
     logFile = open("history.log", "a")
     logFile.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] : {message}\n')
+
+
+def get_subscriber_hash(email):
+    email = email.lower()
+    md5_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
+    return md5_hash
--- remove_from_mailchimp.py
+++ remove_from_mailchimp.py
@@ -6,26 +6,22 @@ junkFile = open('emails.txt', 'r')
 try:
     client = MailchimpMarketing.Client()
     client.set_config({
-    "api_key": "07134cbb1ea260e7dbbdc7a666e0e93a-us7",
+    "api_key": "a798797295e3c50a41ab43ec77a81f6c-us7",
     "server": "us7"
     })
-
-    response = client.automations.list()
+    response = client.lists.get_all_lists()
     while True:
         email = junkFile.readline()
         if not email:
             break
         junkEmail = email.split()[0]
-        for automation in response['automations']:
-            automation_id = automation.get('id', '')
+        for audience in response['lists']:
+            audience_id = audience.get('id', '')
             try:
-                response = client.automations.remove_workflow_email_subscriber(automation_id, {"email_address": junkEmail})
-                if response.status_code == 200:
-                    helper.log(f'success: {junkEmail}')
-                else:
-                    helper.log(f'failed or empty: {junkEmail}')
+                client.lists.delete_list_member_permanent(audience_id, helper.get_subscriber_hash(junkEmail))
+                helper.log(f"Success: {junkEmail}")
             except ApiClientError as error:
-                helper.log("Error: {}".format(error.text))
+                helper.log(f"Failed: {junkEmail}")
 
 except ApiClientError as error:
     helper.log("Error: {}".format(error.text))
