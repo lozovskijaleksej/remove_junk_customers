1112cd03bd701e3e3a63574136716510425a5aef
Senior Shopify Developer <153434083+lozovskijaleksej@users.noreply.github.com>
2023-12-15 14:14:31 +0000

    Update check_status_of_batch_operation.py

--- check_status_of_batch_operation.py
+++ check_status_of_batch_operation.py
@@ -2,12 +2,12 @@ import mailchimp_marketing as MailchimpMarketing
 from mailchimp_marketing.api_client import ApiClientError
 import helper
 
-batch_id = 'cojgq8dsmw'
+batch_id = ''
 
 client = MailchimpMarketing.Client()
 client.set_config({
-"api_key": "83910b2d7916f8275aa94ef881bd4d8f-us7",
-"server": "us7"
+"api_key": "",
+"server": ""
 })
 
 response = client.batches.status(batch_id)
